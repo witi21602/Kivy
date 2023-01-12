024f7ef74e99d770bbb46a98df6b5fcf6e25baf9
witi21602 <anhdaucongo555@gmail.com>
2023-01-12 17:55:08 +0700

    MyGridLayout()

--- Unit01/main.py
+++ Unit01/main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
 
 
-# Press the green button in the gutter to run the script.
+class MyApp(App):
+	def build(self):
+		return Label(text="Hello World", font_size=72)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+	MyApp().run()
